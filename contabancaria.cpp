256efe629065c1a68e9a64e05b4bca5aeaaa7d5e
Maria Fernanda Magalhães Freitas <fernandakicks@gmail.com>
2026-07-22 11:38:32 -0300

    Fix constructor and method naming in Conta class
    Fix constructor initialization and correct method names.

--- contabancaria.cpp
+++ contabancaria.cpp
@@ -2,24 +2,20 @@
 #include <iostream>
 #include <string>
 
-// Construtor da classe Conta, inicialliza os valores dos atributos.
     Conta::Conta(){
         int num = Numero;
         double saldo = Saldo;
         Cliente dono = Titular;
     }
 
-// Simula um depósito em uma conta, adicionando o valor ao saldo atual.
     void Conta::despositar(double Valor){
         Saldo += Valor;
     }
 
-// Simula um saque em uma conta, subtraindo-se o valor do saldo atual.
     void Conta::sacar(double Valor){
         Saldo -= Valor;
     }
 
-// Simula uma transferência entre uma conta origem e uma conta destino, verificando se há saldo sufiiente.
     void Conta::transferir(double Valor, Conta &destino){
         if(Saldo >= Valor){
             destino.Saldo += Valor;
@@ -30,7 +26,6 @@
         }
     }
 
-// Simula uma transferência entre uma conta origem e duas contas de destino, verificando se há saldo sufiiente e dividindo o valor transferido igualmente para ambas.
     void Conta::transferir(double Valor, Conta &destino1, Conta &destino2){
         if(Saldo >= Valor){
             destino1.Saldo += Valor/2;
